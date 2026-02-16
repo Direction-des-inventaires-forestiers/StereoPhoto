86ebd1b4ac5221700c75d82517989b8a0fb77e53
frpin33 <frederickpineault@hotmail.com>
2026-02-16 13:47:21 -0500

    V0.2.0 - Modification GraphicsWindow pour l'affichage
    L'object graphicsWindow qui permet l'affichage des deux images sur le stereoscope a été grandement modifié pour être plus performant. Meilleur gestion du curseur, ajout d'un objet qui gère l'orientation, Viewport avec OpenGl pour utilisé la carte graphique, incorporation de groupe d'item pour les pixmap, fonction pour ajouter un pixmap dans le bon groupe

--- src/ui_graphicsWindow.py
+++ src/ui_graphicsWindow.py
@@ -1,99 +1,158 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ui-graphicsWindow.ui'
-#
-# Created by: PyQt5 UI code generator 5.12.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from qgis.PyQt import QtCore, QtGui, QtWidgets
 from qgis.PyQt.QtCore import Qt
 import os
 
+
+class ImageRoot(QtWidgets.QGraphicsObject):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def boundingRect(self):
+        # Doesn't draw anything, but must exist
+        return QtCore.QRectF()
+
+    def paint(self, painter, option, widget=None):
+        pass
+
+class CursorItem(QtWidgets.QGraphicsItem):
+    def __init__(self, radius=20):
+        super().__init__()
+        self.r = radius
+        self.setZValue(1000)  # ALWAYS on top
+
+        self.setFlags(QtWidgets.QGraphicsItem.ItemIgnoresTransformations)
+        self.setCacheMode(QtWidgets.QGraphicsItem.NoCache)
+
+    def boundingRect(self):
+        pad = 3
+        r = self.r + pad
+        return QtCore.QRectF(-r, -r, 2*r, 2*r)
+
+    def paint(self, painter, option, widget=None):
+        pen = QtGui.QPen(QtGui.QColor(0, 255, 255), 3)
+        painter.setPen(pen)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.drawLine(-self.r, 0, self.r, 0)
+        painter.drawLine(0, -self.r, 0, self.r)
+
 class Ui_graphicsWindow(object):
-    def setupUi(self, graphicsWindow, nom):
+    def setupUi(self, graphicsWindow):
         graphicsWindow.setObjectName("graphicsWindow")
-        graphicsWindow.resize(800, 600)
+
         self.centralwidget = QtWidgets.QWidget(graphicsWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(0, 0, 791, 551))
-        self.graphicsView.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.graphicsView.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.graphicsView.setRenderHint(QtGui.QPainter.SmoothPixmapTransform)
-        self.graphicsView.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(182,182,182)))
-        self.graphicsView.setObjectName("graphicsView")
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(170, 110, 1000, 1200))
-        self.widget.setObjectName("widget")
-        graphicsWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(graphicsWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        graphicsWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(graphicsWindow)
-        self.statusbar.setObjectName("statusbar")
-        graphicsWindow.setStatusBar(self.statusbar)
- 
-        self.retranslateUi(graphicsWindow, nom)
-        QtCore.QMetaObject.connectSlotsByName(graphicsWindow)
-
-    def retranslateUi(self, graphicsWindow, nom):
-        _translate = QtCore.QCoreApplication.translate
-        graphicsWindow.setWindowTitle(_translate("graphicsWindow", nom))
-
-class graphicsWindow(QtWidgets.QMainWindow): 
-    keyPressed = QtCore.pyqtSignal(QtGui.QKeyEvent)
-    def __init__(self, nom):
-        QtWidgets.QMainWindow.__init__(self)
-        self.ui = Ui_graphicsWindow()
-        self.ui.setupUi(self, nom)
-        self.ui.graphicsView.resizeEvent = self.gwResizeEvent
-        self.currentRect = None
-        self.myRect = QtCore.QRect()
-        self.myPen = QtGui.QPen()
-        self.rayon = 20
-        self.ui.widget.paintEvent = self.draw
+        layout = QtWidgets.QVBoxLayout(self.centralwidget)
+        layout.setContentsMargins(0, 0, 0, 0)
         
-        cursorPath =  ":/Anaglyph/Icons/cursor3x3.png"
-        cursorImage = QtGui.QImage(cursorPath)
-        cursorPix = QtGui.QPixmap.fromImage(cursorImage)
-        self.invisibleCursor = QtGui.QCursor(cursorPix)
-        self.normalCursor = self.cursor()
-        #self.setWindowFlags(Qt.Window | Qt.WindowTitleHint | Qt.CustomizeWindowHint | Qt.WindowMaximizeButtonHint | Qt.WindowMinimizeButtonHint)
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+
+        fmt = QtGui.QSurfaceFormat()
+        fmt.setSamples(4)  # Anti-aliasing for the tiles
+        
+        self.gl_viewport = QtWidgets.QOpenGLWidget()
+        self.gl_viewport.setFormat(fmt)
+        self.graphicsView.setViewport(self.gl_viewport)
+
+        
+        self.graphicsView.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.graphicsView.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.graphicsView.setRenderHint(QtGui.QPainter.SmoothPixmapTransform)
+        self.graphicsView.setRenderHint(QtGui.QPainter.Antialiasing)
+        self.graphicsView.setBackgroundBrush(QtGui.QColor(182, 182, 182))
+
+        self.graphicsView.setViewportUpdateMode(QtWidgets.QGraphicsView.FullViewportUpdate)
+        self.graphicsView.setTransformationAnchor(QtWidgets.QGraphicsView.NoAnchor)
+        self.graphicsView.setResizeAnchor(QtWidgets.QGraphicsView.NoAnchor)
+        self.graphicsView.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+
+        layout.addWidget(self.graphicsView)
+        graphicsWindow.setCentralWidget(self.centralwidget)
+
+
+class graphicsWindow(QtWidgets.QMainWindow):
+    keyPressed = QtCore.pyqtSignal(QtGui.QKeyEvent)
+
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_graphicsWindow()
+        self.ui.setupUi(self)
+
+        self.scene = QtWidgets.QGraphicsScene(self)
+        self.scene.setSceneRect(-100000, -100000, 200000, 200000)
+        self.scene.setItemIndexMethod(QtWidgets.QGraphicsScene.NoIndex)
+        self.ui.graphicsView.setScene(self.scene)
+
+        self.resetTileGroup(remove=False)
+
+        self.imageRoot = ImageRoot()
+        self.currentScale = 1
+
+        self.cursor = CursorItem(radius=20)
+        self.scene.addItem(self.cursor)
+
+
 
     def keyPressEvent(self, event):
-        if self.isActiveWindow() : self.keyPressed.emit(event)
-        super().keyPressEvent(event)
+        self.keyPressed.emit(event)
+        #event.accept()
+        #super().keyPressEvent(event)
 
-    def keyReleaseEvent(self, event):
-        if self.isActiveWindow() : self.keyPressed.emit(event)
-        super().keyReleaseEvent(event)
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.centerCrosshair()
+    
+    def showEvent(self, event):
+        super().showEvent(event)
+        self.centerCrosshair()
 
-    #Détermine la position du curseur 
-    def cursorRectInit(self, x, y, extra=0):
-        x = int(x/2) 
-        y = int(y/2)
-        r = self.rayon
-        self.myRect = QtCore.QRect(x-r, y-r, 2*r, 2*r)
-        self.vLine = QtCore.QLineF(x-r, y, x+r, y)
-        self.hLine = QtCore.QLineF(x, y-r, x, y+r)
-        self.ui.widget.update()
+    def centerCrosshair(self):
+        view = self.ui.graphicsView
+        scene_center = view.mapToScene(view.viewport().rect().center())
+        self.cursor.prepareGeometryChange()
+        self.cursor.setPos(scene_center)
 
-    #Fonction qui dessine le curseur au centre de l'image
-    def draw(self, event):
-        p = QtGui.QPainter()
-        p.begin(self.ui.widget)
-        pen = QtGui.QPen(QtGui.QColor(0, 255, 255),3, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin)
-        rect = self.myRect
-        p.setPen(pen)
-        p.setRenderHint(QtGui.QPainter.Antialiasing)
-        #p.drawEllipse(rect)
-        p.drawLine(self.vLine) 
-        p.drawLine(self.hLine)
-        p.end()
+    def custom_centerOn(self,target_scene_pos,scale) :
+        view = self.ui.graphicsView
+        view_center = view.viewport().rect().center()
+        
+        tx = view_center.x() - (target_scene_pos.x() * scale)
+        ty = view_center.y() - (target_scene_pos.y() * scale)
+        
+        transform = QtGui.QTransform()
+        transform.translate(tx, ty)
+        transform.scale(scale, scale)
+        
+        view.setTransform(transform)
+        
+        self.centerCrosshair()
+        
+    def addPixmap(self,pixmap, scaleFactor, topX, topY,groupId) :
+        if groupId == 0 : tileGroup = self.fullviewTileGroup 
+        elif groupId == 1 : tileGroup = self.overviewTileGroup
+        else : tileGroup = self.safetyTileGroup
 
-    def gwResizeEvent(self, event):
-        if self.currentRect :
-            self.ui.graphicsView.fitInView(self.currentRect, Qt.KeepAspectRatio)
-        QtWidgets.QGraphicsView.resizeEvent(self.ui.graphicsView,event)
+        item = QtWidgets.QGraphicsPixmapItem(pixmap,self.imageRoot)
+        item.setPos(topX, topY)
+        item.setScale(scaleFactor)
+        item.setTransformationMode(Qt.FastTransformation) 
+        tileGroup.addToGroup(item)
+
+    def resetTileGroup(self,remove=True) :
+
+        if remove : 
+            self.scene.removeItem(self.fullviewTileGroup)
+            self.scene.removeItem(self.overviewTileGroup)
+            self.scene.removeItem(self.safetyTileGroup)
+
+
+        self.fullviewTileGroup = QtWidgets.QGraphicsItemGroup()
+        self.fullviewTileGroup.setZValue(0)
+        self.scene.addItem(self.fullviewTileGroup)
+
+        self.overviewTileGroup = QtWidgets.QGraphicsItemGroup()
+        self.overviewTileGroup.setZValue(0)
+        self.scene.addItem(self.overviewTileGroup)
+
+        self.safetyTileGroup = QtWidgets.QGraphicsItemGroup()
+        self.safetyTileGroup.setZValue(-100)
+        self.scene.addItem(self.safetyTileGroup)
